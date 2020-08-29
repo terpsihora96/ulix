0f7d23bd2c2f97a2f28c429f4a839242ec2f1e32
terpsihora96 <dzedaj996@gmail.com>
2020-08-29 22:27:05 +0200

    Auth service - login, logout, token manipulation.

---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { Location } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient, private location: Location) {}
+
+  private accessToken: string;
+  private refreshToken: string;
+
+  public login(email: string, password: string): Promise<boolean> {
+    const credentialsPayload = { email, password: btoa(password) };
+    const observable = this.http
+      .post('http://localhost:8080/login', credentialsPayload, {
+        observe: 'response',
+      })
+      .pipe(
+        map((response: any) => {
+          if (response.status === 200) {
+            const body = response.body;
+            this.saveToken(body.access_token, body.refresh_token);
+            location.reload();
+          } else {
+            return false;
+          }
+        }),
+        catchError(() => {
+          return of(false);
+        })
+      );
+
+    return observable.toPromise();
+  }
+
+  public logout(): void {
+    this.accessToken = undefined;
+    this.refreshToken = undefined;
+    localStorage.clear();
+    location.reload();
+    this.location.go('/');
+  }
+
+  private saveToken(accessToken: string, refreshToken: string): void {
+    localStorage.setItem('access-token', accessToken);
+    localStorage.setItem('refresh-token', refreshToken);
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
+
+  public getAccessToken(): string {
+    return localStorage.getItem('access-token');
+  }
+}
