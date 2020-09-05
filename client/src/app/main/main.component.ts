f275254cf2489052c0a971b20819d2f5bfacf275
terpsihora96 <dzedaj996@gmail.com>
2020-09-05 22:48:38 +0200

    First look of main component created.

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  categories = [
+    { title: '1', topics: [{ title: 'topic1' }] },
+    { title: '2', topics: [{ title: 'topic2' }] },
+    { title: '2', topics: [{ title: 'topic2' }] },
+    { title: '2', topics: [{ title: 'topic2' }] },
+    { title: '2', topics: [{ title: 'topic2' }] },
+    { title: '2', topics: [{ title: 'topic2' }] },
+  ];
+  panelOpenState = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
