2c720201dc62aee35b6fef37e12aff533aa2512f
terpsihora96 <dzedaj996@gmail.com>
2020-09-06 19:53:47 +0200

    Guard for protecting the route from unauthenticated users; redirects if user is not authenticated.

---
+++ client/src/app/services/guards/auth-guard.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Location } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(public auth: AuthService, private location: Location) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.auth.getAccessToken()) {
+      return true;
+    }
+    this.location.go('login');
+    location.reload();
+    return false;
+  }
+}
